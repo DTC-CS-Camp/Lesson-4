d9f10ec761d142d15275435adf46a4262c9b2f3d
DTC-CS-Camp <68837059+DTC-CS-Camp@users.noreply.github.com>
2021-08-02 16:12:25 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,28 @@
+class Main {
+  public static void main
+
+  // code problem 1
+  String name= "Mario";
+
+
+
+
+
+
+  // code problem 2
+  String str= "redxx";
+
+
+
+
+
+
+  // code problem 3
+  String input= "bruh";
+
+
+
+
+
+  }
+}
